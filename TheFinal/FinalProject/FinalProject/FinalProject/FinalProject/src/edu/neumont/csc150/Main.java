781e6288a554e523c4e9432d47085b5674b10d98
fakesaltybags <124113268+fakesaltybags@users.noreply.github.com>
2023-03-02 15:49:09 -0700

    Delete TheFinal directory

--- TheFinal/FinalProject/FinalProject/FinalProject/FinalProject/src/edu/neumont/csc150/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.neumont.csc150;
-
-import edu.neumont.csc150.controllers.GameController;
-
-public class Main {
-    public static void main(String[] args) {
-        new GameController().run();
-    }
-}
